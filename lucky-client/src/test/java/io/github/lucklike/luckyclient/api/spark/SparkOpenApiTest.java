d91419afcadeb57cdf025adb39997ce8957e39a2
fukang1163 <fukang1163@cairenhui.com>
2024-07-16 10:20:32 +0800

    20240716 API配置改造

--- lucky-client/src/test/java/io/github/lucklike/luckyclient/api/spark/SparkOpenApiTest.java
+++ lucky-client/src/test/java/io/github/lucklike/luckyclient/api/spark/SparkOpenApiTest.java
@@ -24,15 +24,15 @@ public class SparkOpenApiTest {
 
     @Test
     void idCardOcrTest() {
-        Map<String, Object> idCardInfo = sparkOpenApi.idCardOcr("file:/Users/fukang/Desktop/WechatIMG6146.jpg");
+        Map<String, Object> idCardInfo = sparkOpenApi.idCardOcr("file:D:/id/card/20240716100959.jpg");
         System.out.println(idCardInfo);
     }
 
     @Test
     void imageGenerateTest() throws IOException {
         String imageBase64 = sparkOpenApi.imageGenerate("宇宙大爆炸");
-        String fileName = NanoIdUtils.randomNanoId() + ".jpg";;
-        File file = new File("/Users/fukang/Desktop/" + fileName);
+        String fileName = NanoIdUtils.randomNanoId() + ".jpg";
+        File file = new File("D:/test/" + fileName);
         FileCopyUtils.copy(Base64.decode(imageBase64), file);
         System.out.println(file.getAbsolutePath());
     }
