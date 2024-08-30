c6b0eb6c98a0cdf46770ace905eff4856924c84c
fukang1163 <fukang1163@cairenhui.com>
2024-08-30 20:17:11 +0800

    20240830 Mock-SSE

--- lucky-client/src/main/java/io/github/lucklike/luckyclient/api/baiduai/BaiduAI.java
+++ lucky-client/src/main/java/io/github/lucklike/luckyclient/api/baiduai/BaiduAI.java
@@ -1,12 +1,21 @@
 package io.github.lucklike.luckyclient.api.baiduai;
 
+import com.luckyframework.httpclient.core.meta.Request;
+import com.luckyframework.httpclient.core.meta.Response;
 import com.luckyframework.httpclient.generalapi.token.JsonFileTokenManager;
+import com.luckyframework.httpclient.proxy.CommonFunctions;
+import com.luckyframework.httpclient.proxy.context.MethodContext;
+import com.luckyframework.httpclient.proxy.mock.Mock;
+import com.luckyframework.httpclient.proxy.mock.MockResponse;
 import io.github.lucklike.httpclient.annotation.HttpClientComponent;
 import io.github.lucklike.luckyclient.api.util.AI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 
 
 /**
@@ -27,8 +36,22 @@ public interface BaiduAI extends AI {
      *
      * @param content 提问内容
      */
+    @Mock(mockResp = "#{#questionsAndAnswersMock($mc$, $req$)}", cache = false)
     void questionsAndAnswers(String content);
 
+    static Response questionsAndAnswersMock(MethodContext context, Request request) throws IOException {
+        String sb =
+                "data: {\"is_end\": false, \"result\": \"ARGS: "+context.getArguments()[0]+"\\n\"}\n\n" +
+                "data: {\"is_end\": false, \"result\": \"URL: ["+request.getRequestMethod()+"] "+request.getUrl()+"\\n\"}\n\n" +
+                "data: {\"is_end\": false, \"result\": \"你好呀\"}\n\n" +
+                "data: {\"is_end\": false, \"result\": \",我是Mock出来的数据\"}\n\n" +
+                "data: {\"is_end\": false, \"result\": \"。我用于为方法questionsAndAnswers提供测试数据!\"}\n\n" +
+                "data: {\"is_end\": true, \"result\": \"！\"}\n\n";
+        return MockResponse
+                .create()
+                .header("Content-Type", "text/event-stream; charset=utf-8")
+                .body(new ByteArrayInputStream(sb.getBytes()));
+    }
 
     /**
      * Token管理器
