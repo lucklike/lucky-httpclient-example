9b65a9b6e8f63401a203d387d11a8f850e94958f
fukang1163 <fukang1163@cairenhui.com>
2024-11-11 20:08:05 +0800

    20241111 v2版本分片文件下载测试

--- lucky-client/src/test/java/io/github/lucklike/luckyclient/api/inject/DownloadTes.java
+++ lucky-client/src/test/java/io/github/lucklike/luckyclient/api/inject/DownloadTes.java
@@ -1,6 +1,8 @@
 package io.github.lucklike.luckyclient.api.inject;
 
-import com.luckyframework.httpclient.generalapi.RangeDownloadApi;
+import com.luckyframework.httpclient.core.meta.Request;
+import com.luckyframework.httpclient.generalapi.Range;
+import com.luckyframework.httpclient.generalapi.RangeDownloadApi2;
 import io.github.lucklike.httpclient.annotation.HttpReference;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,8 +22,29 @@ public class DownloadTes {
 
     @Test
     void download() throws Exception {
-        String url = "https://github.com/docmirror/dev-sidecar/releases/download/v1.8.9/DevSidecar-1.8.9-node16.dmg";
-        File file = downloadApi.rangeFileDownload(url, "/Users/fukang/Downloads/dev-sidecar");
+        String url = "https://github.com/docmirror/dev-sidecar/releases/download/v1.8.9/DevSidecar-1.8.9-node16.exe";
+        File file = downloadApi.rangeFileDownload(url, "D:/test/dev-sidecar", 2 * 1024 * 1024);
         System.out.println(file.getAbsolutePath());
+    }
+
+    @HttpReference
+    RangeDownloadApi2 downloadApi2;
+
+    @Test
+    void rangeTest() {
+        Request request = Request
+                .head("https://mirrors.aliyun.com/centos-stream/10-stream/BaseOS/x86_64/iso/CentOS-Stream-10-20241104.0-x86_64-boot.iso")
+                .setConnectTimeout(120000)
+                .setWriterTimeout(60000)
+                .setReadTimeout(60000);
+        Range rangeInfo = downloadApi2.rangeInfo(request);
+        System.out.println(rangeInfo);
+    }
+
+    @Test
+    void rangeDownloadTest() {
+        Request request = Request.get("https://mirrors.aliyun.com/centos/8/isos/x86_64/CentOS-8.5.2111-x86_64-boot.iso");
+        File file = downloadApi2.rangeFileDownload(request, "D:/test/iso/", null, RangeDownloadApi2.DEFAULT_RANGE_SIZE);
+        System.out.println(file);
     }
 }
--- lucky-client/src/test/java/io/github/lucklike/luckyclient/api/inject/MyDownloadApi.java
+++ lucky-client/src/test/java/io/github/lucklike/luckyclient/api/inject/MyDownloadApi.java
@@ -1,6 +1,7 @@
 package io.github.lucklike.luckyclient.api.inject;
 
 import com.luckyframework.httpclient.generalapi.RangeDownloadApi;
+import com.luckyframework.httpclient.proxy.annotations.BrowserFeign;
 import com.luckyframework.httpclient.proxy.annotations.HttpExec;
 import com.luckyframework.httpclient.proxy.annotations.PrintLogProhibition;
 import com.luckyframework.httpclient.proxy.annotations.PrintRequestLog;
@@ -12,7 +13,8 @@ import com.luckyframework.httpclient.proxy.annotations.Timeout;
  * @date 2024/11/8 01:41
  */
 @HttpExec.http_client
-@Timeout(readTimeout = 3 * 6000)
-@PrintLogProhibition
+@Timeout(readTimeout = 5 * 6000, connectionTimeout = 2 * 6000)
+@PrintRequestLog
+@BrowserFeign
 public interface MyDownloadApi extends RangeDownloadApi {
 }
