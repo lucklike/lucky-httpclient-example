0476374a1e5831710118831fee0b191d9ac32bad
fk7075 <1814375626@qq.com>
2025-01-21 03:28:55 +0800

    20250121 分片文件上传客户端代码优化

--- lucky-client/src/main/java/io/github/lucklike/luckyclient/api/server/ann/FileChunkUploadApi.java
+++ lucky-client/src/main/java/io/github/lucklike/luckyclient/api/server/ann/FileChunkUploadApi.java
@@ -1,17 +1,31 @@
 package io.github.lucklike.luckyclient.api.server.ann;
 
+import com.luckyframework.common.ContainerUtils;
+import com.luckyframework.httpclient.generalapi.chunk.FileChunk;
+import com.luckyframework.httpclient.generalapi.chunk.FileChunkHandle;
 import com.luckyframework.httpclient.generalapi.describe.Describe;
+import com.luckyframework.httpclient.proxy.CommonFunctions;
 import com.luckyframework.httpclient.proxy.annotations.Get;
 import com.luckyframework.httpclient.proxy.annotations.MultiData;
 import com.luckyframework.httpclient.proxy.annotations.MultiFile;
 import com.luckyframework.httpclient.proxy.annotations.Post;
 import com.luckyframework.httpclient.proxy.annotations.QueryParam;
 import io.github.lucklike.httpclient.annotation.HttpClient;
+import lombok.SneakyThrows;
+import org.springframework.lang.NonNull;
 
+import java.io.File;
 import java.util.Map;
 
+/**
+ * 分片文件上传API
+ */
 @HttpClient("http://localhost:8864/shard/upload")
 public interface FileChunkUploadApi {
+
+    //-----------------------------------------------------------------
+    //                          Http Api
+    //-----------------------------------------------------------------
 
 
     @Get("uploadChunks")
@@ -26,4 +40,65 @@ public interface FileChunkUploadApi {
     @Post("/merge")
     @Describe("通知服务器进行文件合并")
     String mergeChunks(@QueryParam String fileId, @QueryParam String fileName);
+
+    //-----------------------------------------------------------------
+    //                         Upload Method
+    //-----------------------------------------------------------------
+
+    /**
+     * 分片文件上传
+     *
+     * @param file           文件对象
+     * @param chunkSize      分片大小
+     * @param maxConcurrency 最大并发数
+     * @throws Exception 上传过程中可能出现的异常
+     */
+    default void uploadFile(File file, long chunkSize, int maxConcurrency) throws Exception {
+
+        // 获取文件名和文件hash
+        String fileId = CommonFunctions.md5Hex(file);
+        String fileName = file.getName();
+
+        // 获取已经上传到服务器中的分片文件信息
+        Map<Integer, String> uploadChunkMap = uploadChunks(fileId);
+
+        // 获取分片文件处理器进行处理
+        FileChunkHandle handle = FileChunkHandle.of(file, chunkSize);
+        handle.asyncHandle(
+                sf -> {
+                    if (isUploaded(uploadChunkMap, sf)) {
+                        return "ok";
+                    } else {
+                        return uploadChunk(fileId, sf.getIndex(), sf.getContent());
+                    }
+                },
+                rs -> {
+                    String r = mergeChunks(fileId, fileName);
+                    rs.forEach(System.out::println);
+                    System.out.println(r);
+
+                },
+                maxConcurrency
+        );
+    }
+
+    /**
+     * 检查否个分片文件是否已经上传
+     *
+     * @param chunkMap  服务器上已有的分片文件信息
+     * @param fileChunk 带检查的分片文件
+     * @return 是否已经上传到服务器了
+     */
+    @SneakyThrows
+    default boolean isUploaded(Map<Integer, String> chunkMap, @NonNull FileChunk fileChunk) {
+        if (ContainerUtils.isEmptyMap(chunkMap)) {
+            return false;
+        }
+        String hash = chunkMap.get((int) fileChunk.getIndex());
+        if (hash == null) {
+            return false;
+        }
+
+        return hash.equals(CommonFunctions.md5Hex(fileChunk.getContent()));
+    }
 }
--- lucky-client/src/test/java/io/github/lucklike/luckyclient/api/server/ann/FileChunkUploadApiTest.java
+++ lucky-client/src/test/java/io/github/lucklike/luckyclient/api/server/ann/FileChunkUploadApiTest.java
@@ -1,22 +1,12 @@
 package io.github.lucklike.luckyclient.api.server.ann;
 
-import com.luckyframework.common.ContainerUtils;
-import com.luckyframework.conversion.ConversionUtils;
-import com.luckyframework.httpclient.generalapi.chunk.FileChunk;
-import com.luckyframework.httpclient.generalapi.chunk.FileChunkHandle;
 import com.luckyframework.httpclient.proxy.CommonFunctions;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.lang.NonNull;
 
 import javax.annotation.Resource;
-
 import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @SpringBootTest
@@ -28,42 +18,7 @@ class FileChunkUploadApiTest {
     @Test
     void chunkUploadTest() throws Exception {
         File file = new File("/Users/fukang/Lucky/lucky-httpclient-example/lucky-client/src/main/resources/application.yml");
-        String fileId = CommonFunctions.md5Hex(file);
-        String fileName = file.getName();
-
-        Map<Integer, String> chunkMap = api.uploadChunks(fileId);
-        FileChunkHandle handle = FileChunkHandle.of(file, 100);
-        handle.asyncHandle(
-                sf -> {
-                    if (isUploaded(chunkMap, sf)) {
-                        return "ok";
-                    } else {
-                        return api.uploadChunk(fileId, sf.getIndex(), sf.getContent());
-                    }
-                },
-                rs -> {
-                    String r = api.mergeChunks(fileId, fileName);
-                    rs.forEach(System.out::println);
-                    System.out.println(r);
-
-                },
-                10
-        );
-
-        System.out.println(fileId);
-    }
-
-    @SneakyThrows
-    public boolean isUploaded(Map<Integer, String> chunkMap, @NonNull FileChunk fileChunk) {
-        if (ContainerUtils.isEmptyMap(chunkMap)) {
-            return false;
-        }
-        String hash = chunkMap.get((int) fileChunk.getIndex());
-        if (hash == null) {
-            return false;
-        }
-
-        return hash.equals(CommonFunctions.md5Hex(fileChunk.getContent()));
+        api.uploadFile(file, 100L, 10);
     }
 
     @Test
