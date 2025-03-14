36e005b7b2f2f49db2c31d7d819d9b159716e80c
fk7075 <1814375626@qq.com>
2025-03-15 04:22:25 +0800

    20250315 优化基于SpringMultipartFile的文件上传接口测案例

--- lucky-server/src/main/java/io/github/lucklike/server/ai/crh/ElecagreemodelApi.java
+++ lucky-server/src/main/java/io/github/lucklike/server/ai/crh/ElecagreemodelApi.java
@@ -1,13 +1,12 @@
 package io.github.lucklike.server.ai.crh;
 
 import com.luckyframework.httpclient.proxy.annotations.HeaderParam;
-import com.luckyframework.httpclient.proxy.annotations.MultiData;
 import com.luckyframework.httpclient.proxy.annotations.MultiFile;
 import com.luckyframework.httpclient.proxy.annotations.Post;
 import com.luckyframework.httpclient.proxy.annotations.PrintLog;
 import com.luckyframework.httpclient.proxy.annotations.QueryParam;
 import io.github.lucklike.httpclient.discovery.HttpClient;
-import org.springframework.core.io.InputStreamSource;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -15,6 +14,11 @@ import java.util.Map;
 @HttpClient("http://localhost:8015/elecagreemodel/")
 public interface ElecagreemodelApi {
 
-    @Post("fundSumaryPublish?agreement_type=#{agreement_type}")
-    Map<String, Object> fundSumaryPublish(@HeaderParam String authorization, String agreement_type, @MultiFile Map<String, InputStreamSource> files);
+    @Post("fundSumaryPublish")
+    Map<String, Object> fundSumaryPublish(
+            @HeaderParam String authorization,
+            @QueryParam String agreement_type,
+            @MultiFile MultipartFile[] files
+    );
+
 }
--- lucky-server/src/main/java/io/github/lucklike/server/controller/ElecagreemodelController.java
+++ lucky-server/src/main/java/io/github/lucklike/server/controller/ElecagreemodelController.java
@@ -1,8 +1,6 @@
 package io.github.lucklike.server.controller;
 
-import com.luckyframework.httpclient.proxy.annotations.HeaderParam;
 import io.github.lucklike.server.ai.crh.ElecagreemodelApi;
-import org.springframework.core.io.InputStreamSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 @CrossOrigin
@@ -24,10 +21,6 @@ public class ElecagreemodelController {
 
     @RequestMapping("fundSumaryPublish")
     public Map<String, Object> fundSumaryPublish(@RequestHeader("authorization") String token, @RequestParam String agreement_type, MultipartFile[] files) {
-        Map<String, InputStreamSource> map = new LinkedHashMap<>();
-        for (MultipartFile file : files) {
-            map.put(file.getOriginalFilename(), file);
-        }
-        return api.fundSumaryPublish(token, agreement_type, map);
+        return api.fundSumaryPublish(token, agreement_type, files);
     }
 }
